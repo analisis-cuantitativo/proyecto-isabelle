075d989340162ca5429e9416ec61cb67ab06d202
Miguel González Duque <miguelgondu@gmail.com>
2026-03-20 16:57:55 -0500

    implementa una función para guardar en thy

--- src/proyecto_isabelle/parse/thy.py
+++ src/proyecto_isabelle/parse/thy.py
@@ -11,8 +11,20 @@ def load_text(path: str | Path) -> str:
     """
     fp = Path(path)
 
-
     if not fp.is_file():
         raise FileNotFoundError(f"El archivo {path} no existe.")
-    
-    return fp.read_text(encoding="utf-8") #formato de texto utf-8 para evitar problemas con caracteres especiales
+
+    return fp.read_text(
+        encoding="utf-8"
+    )  # formato de texto utf-8 para evitar problemas con caracteres especiales
+
+
+def save_text(text: str, path: str | Path) -> None:
+    """
+    Esta función guarda el texto en un archivo en path
+    especificado.
+    """
+
+    # Se crea el archivo en la ruta correspondiente si no existe y se escribe en el"
+    with open(path, "w", encoding="utf-8") as archivo:
+        archivo.write(text)
