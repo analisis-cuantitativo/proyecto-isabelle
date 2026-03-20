da2229cf208277dcd20925f620e0f6d96b723f72
jacoval311 <jacoval31@gmail.com>
2026-03-20 16:39:49 -0500

    función de cargar texto

--- src/proyecto_isabelle/parse/thy.py
+++ src/proyecto_isabelle/parse/thy.py
@@ -4,5 +4,15 @@ from pathlib import Path
 def load_text(path: str | Path) -> str:
     """
     Carga un archivo .thy en el path especificado.
+
+    recibimos la dirección
+    validamos que exista y que sea un archivo
+    si está bien, se lee el contenido del archivo y se devuelve como una cadena de texto
     """
-    ...
+    fp = Path(path)
+
+
+    if not fp.is_file():
+        raise FileNotFoundError(f"El archivo {path} no existe.")
+    
+    return fp.read_text(encoding="utf-8") #formato de texto utf-8 para evitar problemas con caracteres especiales
